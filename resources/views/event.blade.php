76223b019fa694b541a4842e6a8553877beeb25c
ouassahoulou <reda.ouassahoulou@gmail.com>
2020-05-26 15:10:53 +0000

    init

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -9,7 +9,7 @@
             @csrf
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); "for="titre"><strong>{{__('Titre')}}</strong></label>
-            <input type="text" name="titre" id="titre" class="form-control ">
+            <input type="text" name="titre" id="titre" class="form-control " required>
             </div>
             <div class="form-group">
                 <label  style="color: rgb(228, 241, 250); " for="titre"><strong>{{__('Type')}}</strong></label>
@@ -20,13 +20,13 @@
                 </div>
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); "  for="locale"><strong>{{__('Locale')}}</strong></label>
-            <input type="text" name="locale" id="locale" class="form-control ">
+            <input type="text" name="locale" id="locale" class="form-control " required>
             </div>
             <label style="color: rgb(228, 241, 250); " for="duree"><strong>{{__('Duree')}}</strong></label>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     
-                    <input type="text" name="number" id="number" class="form-control" placeholder="Nombre">
+                    <input type="text" name="number" id="number" class="form-control" placeholder="Nombre" required>
                 </div>
                 <div class="form-group col-md-6">
                     <select name="timing" id="timing" class="form-control">
@@ -38,35 +38,35 @@
             </div>   
             <div class="form-group">       
             <label style="color: rgb(228, 241, 250); "  for="date"><strong>{{__('Date')}}</strong></label>
-            <input type="date" name="date" id="date" class="form-control ">
+            <input type="date" name="date" id="date" class="form-control " required>
             </div>
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); " for="heure"><strong>{{__('Heure')}}</strong></label>
-            <input type="time" name="heure" id="heure" class="form-control ">
+            <input type="time" name="heure" id="heure" class="form-control " required>
             </div>
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); " for="plan"><strong>{{__('Plan')}}</strong></label>
-            <textarea name="plan" id="plan" class="form-control "></textarea>
+            <textarea name="plan" id="plan" class="form-control " required></textarea>
             </div>
             <div class="form-group">
                 <label style="color: rgb(228, 241, 250); " for="description"><strong>{{__('Description')}}</strong></label>
-                <textarea name="description" id="description" class="form-control "></textarea>
+                <textarea name="description" id="description" class="form-control " required></textarea>
                 </div>
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); "  for="nb_place"><strong>{{__('Nombre de place')}}</strong></label>
-            <input type="number" name="nb_place" id="nb_place" class="form-control ">
+            <input type="number" name="nb_place" id="nb_place" class="form-control " required>
             </div>
             <div class="form-group">
             <label style="color: rgb(228, 241, 250); " for="nb_pause"><strong>{{__('Nombre de pause')}}</strong></label>
-            <input type="number" name="nb_pause" id="nb_pause" class="form-control ">
+            <input type="number" name="nb_pause" id="nb_pause" class="form-control " required>
             </div>
             <div class="form-group">
                 <label  style="color: rgb(228, 241, 250); " for="nb_animateur"><strong>{{__("Nombre d'animateur")}}</strong></label>
-                <input type="number" name="nb_animateur" id="nb_animateur" class="form-control ">
+                <input type="number" name="nb_animateur" id="nb_animateur" class="form-control " required>
             </div>  
             <div class="form-group">
                 <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="customCheck1" name="free">
+                    <input type="checkbox" class="custom-control-input" id="customCheck1" name="free" required>
                     <label style="color: rgb(228, 241, 250); "  class="custom-control-label" for="customCheck1">{{__('free')}}</label>
                   </div>
             </div>

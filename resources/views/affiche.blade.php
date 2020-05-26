64b49dc276cbac789351ea08ad8853773d5dcca2
ouassahoulou <reda.ouassahoulou@gmail.com>
2020-05-26 16:11:36 +0000

    init

--- resources/views/affiche.blade.php
+++ resources/views/affiche.blade.php
@@ -4,7 +4,7 @@
 <body> 
     
         <img src='{{asset($aff)}}' alt='image' style="width: 188.4mm; height: 266mm;">
-        {{-- style="width: 21cm; height: 29.7cm;" --}}
+        {{-- style="width: 21cm; height: 29.7cm;"aaa --}}
     
 </body>
 </html>
